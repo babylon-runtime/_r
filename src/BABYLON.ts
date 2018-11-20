1a42afcf83cf223fa18410755466ca6e372adcc3
fabsharp <Omahabitch14>
2018-11-20 15:28:39 +0100

    add comment

--- src/BABYLON.ts
+++ src/BABYLON.ts
@@ -1,3 +1,7 @@
-import * as BABYLON from "../node_modules/babylonjs/es6.js";
+// this will let us :
+// - have autocompletion in IDE (PHPStorm here)
+// - test easily with import * from index.js
+// ...but it's not including in bundle by rollup.
 
+import * as BABYLON from "../node_modules/babylonjs/es6.js";
 export {BABYLON};
