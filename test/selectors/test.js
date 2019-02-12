b6fa3fb31c5195bda940412c3f5b933d7f2b3769
fabsharp <Omahabitch14>
2019-02-12 11:05:06 +0100

    matchFilters bug

--- test/selectors/test.js
+++ test/selectors/test.js
@@ -1,11 +1,27 @@
 var expect = chai.expect;
 
-describe('Test types', function() {
-    describe('Meshes', function() {
-        it('_r.select(,*:mesh).length ==', function() {
-            expect(_r.scene).to.exist;
-            // expect(_r.scene.clearColor.r === 1).to.be.true;
-            // expect(_r.select("light.000")[0].emissiveColor.r === 1).to.be.true;
+describe('Selectors', function() {
+    describe('Basic', function() {
+        it('_r.select("*:mesh").length === 4', function() {
+            expect(_r.select("*:mesh").length === 4).to.be.true;
+        });
+        it('_r.select("*.cornellBox.*:material").length === 4', function() {
+            expect(_r.select("*.cornellBox.*:material").length === 4).to.be.true;
+        });
+        it('_r.select("cornellBox.*:material").length === 7', function() {
+            expect(_r.select("cornellBox.*:material").length === 7).to.be.true;
+        });
+        it('_r.select("*bloc*:material").length === 1', function() {
+            expect(_r.select("*bloc*:material").length === 1).to.be.true;
+        });
+        it('_r.select("bloc*:material").length === 0', function() {
+            expect(_r.select("bloc*:material").length === 0).to.be.true;
+        });
+        it('_r.select("*:camera").length === 1', function() {
+            expect(_r.select("*:camera").length === 1).to.be.true;
+        });
+        it('_r.select("*:light").length === 1', function() {
+            expect(_r.select("*:light").length === 1).to.be.true;
         });
     });
 });
