cc1d9b3834249f7c2ea8d0653685b46de62f78fd
fabsharp <Omahabitch14>
2020-01-10 11:33:08 +0100

    npm-publish

---
+++ tools/npm-publish/README.md
@@ -0,0 +1,7 @@
+# Babylon Runtime
+A lightweight, *write less, do more*, [BabylonJS](https://www.babylonjs.com/) library.
+## Code less, Babylon more
+
+<img src="https://raw.githubusercontent.com/babylon-runtime/_r.assets/master/_runtime-logo/exports/_runtime-logo_circleWhite_512.png" alt="babylon runtime logo" width="250" >
+
+Demo, API & Examples : [https://babylon-runtime.github.io/](https://babylon-runtime.github.io/)
---
+++ tools/npm-publish/prebuildpackage.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const pkg = require("../../package.json");
+const mkdirp = require("mkdirp");
+
+pkg.scripts = {};
+pkg.devDependencies = {};
+pkg.main = '_r.js';
+
+mkdirp('dist.npm');
+fs.writeFileSync("dist.npm/package.json", Buffer.from(JSON.stringify(pkg, null, 2), "utf-8") );
+fs.copyFileSync(__dirname + "/README.md", "dist.npm/README.md");
+fs.copyFileSync("dist/_r.js", "dist.npm/_r.js");
+fs.copyFileSync("dist/_r.min.js", "dist.npm/_r.min.js");
+fs.copyFileSync("dist/_r.js", "dist.npm/_r.js.map");
+fs.copyFileSync("dist/_r.min.js", "dist.npm/_r.min.js");
+fs.copyFileSync("dist/_r.min.js", "dist.npm/_r.min.js.map");
+console.log(pkg);
