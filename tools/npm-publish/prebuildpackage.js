202068aae17efccf8fe5671a6d0957669c5af7ef
fabsharp <Omahabitch14>
2020-01-10 12:01:09 +0100

    npm-publish

--- tools/npm-publish/prebuildpackage.js
+++ tools/npm-publish/prebuildpackage.js
@@ -8,10 +8,10 @@ pkg.main = '_r.js';
 
 mkdirp('dist.npm');
 fs.writeFileSync("dist.npm/package.json", Buffer.from(JSON.stringify(pkg, null, 2), "utf-8") );
-fs.copyFileSync(__dirname + "/README.md", "dist.npm/README.md");
+fs.copyFileSync("README.md", "dist.npm/README.md");
 fs.copyFileSync("dist/_r.js", "dist.npm/_r.js");
 fs.copyFileSync("dist/_r.min.js", "dist.npm/_r.min.js");
 fs.copyFileSync("dist/_r.js", "dist.npm/_r.js.map");
 fs.copyFileSync("dist/_r.min.js", "dist.npm/_r.min.js");
 fs.copyFileSync("dist/_r.min.js", "dist.npm/_r.min.js.map");
-console.log(pkg);
+console.log('package ready for npmjs.com');
