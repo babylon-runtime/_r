0c8581969936b5caf690c7d7beadbee7496d00bf
fabsharp <Omahabitch14>
2020-02-21 17:49:51 +0100

    0.1.10

--- dist/_r.js
+++ dist/_r.js
@@ -4,7 +4,7 @@
   (global = global || self, global._r = factory());
 }(this, (function () { 'use strict';
 
-  console.log("babylon runtime v0.1.9")
+  console.log("babylon runtime v0.1.10")
 
   var is;
   (function (is) {
